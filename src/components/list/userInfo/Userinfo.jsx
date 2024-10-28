86b07db41700b345eaa0ee0459016e531bf7b925
batumzff <batuhanmuzafferoglu@gmail.com>
2024-10-28 11:38:48 +0300

    updated: get ChatList

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -1,19 +1,22 @@
-import "./userInfo.css"
+import "./userInfo.css";
+import { useUserStore } from "../../../lib/userStore";
 
 const Userinfo = () => {
-  return (
-    <div className='userInfo'>
-        <div className="user">
-            <img src="./avatar.png" alt="" />
-            <h2>Batuhan Muzafferoğlu</h2>
-        </div>
-        <div className="icons">
-            <img src="./more.png" alt="" />
-            <img src="./video.png" alt="" />
-            <img src="./edit.png" alt="" />
-        </div>
-    </div>
-  )
-}
+  const { currentUser } = useUserStore();
 
-export default Userinfo
+  return (
+    <div className="userInfo">
+      <div className="user">
+        <img src={currentUser.avatar || "./avatar.png"} alt="" />
+        <h2>{currentUser.username}</h2>
+      </div>
+      <div className="icons">
+        <img src="./more.png" alt="" />
+        <img src="./video.png" alt="" />
+        <img src="./edit.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Userinfo;
